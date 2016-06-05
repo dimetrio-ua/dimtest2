2ae72f7ac4fb77acb0e925afa5d82181c019e345
Dmytro Botnarenko <dmytro.botnarenko@gmail.com>
2016-06-05 17:52:16 +0300

    npm optimization plugins
    added Normalize.css
    added npm-check-updates (global)
    added postcss-import & css-mqpacker & cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var atImport = require('postcss-import');
+var mqpacker = require('css-mqpacker');
 var autoprefixer = require('autoprefixer');
 var color_rgba_fallback = require('postcss-color-rgba-fallback');
 var opacity = require('postcss-opacity');
@@ -7,16 +9,20 @@ var pseudoelements = require('postcss-pseudoelements');
 var vmin = require('postcss-vmin');
 var pixrem = require('pixrem');
 var precss = require('precss');
+var cssnano = require('cssnano');
 
 gulp.task('css', function () {
   var processors = [
+    atImport,
+    mqpacker,
     precss,
     color_rgba_fallback,
     opacity,
     pseudoelements,
     vmin,
-    pixrem,
-    autoprefixer
+    pixrem ({atrules: true}),
+    autoprefixer,
+    cssnano
   ];
   return gulp.src('./src/css/*.css')
     .pipe(postcss(processors))
